b664ab4b0f35e85916e664faf938716f445f7928
akash <akashes5753279@gmail.com>
2025-05-12 09:35:41 +0530

    feat:add responsive navbar component

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,67 @@
+import React, { useState } from 'react'
+import '../componentStyles/Navbar.css'
+import { Link } from 'react-router-dom'
+import SearchIcon from '@mui/icons-material/Search'
+import { ShoppingCart } from '@mui/icons-material'
+import  PersonAddIcon from '@mui/icons-material/PersonAdd'
+import  CloseIcon from '@mui/icons-material/Close'
+import MenuIcon from '@mui/icons-material/Menu'
+
+
+const Navbar = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false)
+    const toggleMenu=()=>setIsMenuOpen(!isMenuOpen)
+
+    const isAuthenticated = true
+  return (
+    <nav className="navbar">
+        <div className="navbar-container">
+            <div className="navbar-logo">
+                <Link to='/' onClick={()=>setIsMenuOpen(false)}>ShopEasy</Link>
+            </div>
+            <div className={`navbar-links ${isMenuOpen ? 'active' : ''}`}>
+                <ul>
+                    <li  onClick={()=>setIsMenuOpen(false)} ><Link to='/home'>Home</Link></li>
+                    <li><Link to='/products'>Products</Link></li>
+                    <li><Link to='/about-us'>About Us</Link></li>
+                    <li><Link to='/contact-us'>Contact Us</Link></li>
+                </ul>
+            </div>
+            <div className="navbar-icons">
+                {/* <div className="search-container">
+                    <form action="" className="search-form">
+                        <input type="text" className='search-input'
+                        placeholder='search-products...'
+                        />
+                        <button className="search-icon">
+                            <SearchIcon focusable='false'/>
+                        </button>
+                    </form>
+
+                </div> */}
+                <div className="cart-container">
+                    <Link to='/cart'>
+                    <ShoppingCart className='icon'/>
+                    <span className="cart-badge">6</span>
+                        </Link>
+                </div>
+                {
+                !isAuthenticated &&  <Link to='/register' className='register-link'><PersonAddIcon className='icon'/></Link>
+                   
+                }
+                <div className="navbar-hamburger" onClick={toggleMenu}>
+                    {
+                        isMenuOpen?(
+                            <CloseIcon className='icon'/>
+                        ):(
+                            <MenuIcon className='icon'/>
+                        )
+                    }
+                </div>
+            </div>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,10 @@
 import Footer from '../components/Footer'
+import Navbar from '../components/Navbar'
 import '../pageStyles/Home.css'
 const Home = () => {
   return (
    <>
+   <Navbar/>
     <div className="home-container">
       <h2 className="home-heading">Trending Now</h2>
 
