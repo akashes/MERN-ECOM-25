894bb4b2e26d0a436d85df95ec2217a61ab6185d
akash <akashes5753279@gmail.com>
2025-05-12 15:58:23 +0530

    feat:add rating component which we can optionally disable,add page title reusable component which changes page title with pages

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,14 +1,64 @@
 import Footer from '../components/Footer'
 import ImageSlider from '../components/ImageSlider'
 import Navbar from '../components/Navbar'
+import PageTitle from '../components/PageTitle'
+import Product from '../components/Product'
 import '../pageStyles/Home.css'
+const products=[
+   {
+            "_id": "6818ddbba199ed8135a62be8",
+            "name": "charger",
+            "description": "18 w fast charger",
+            "price": 2000,
+            "ratings": 2,
+            "category": "electronics",
+            "stock": 96,
+            "numOfReviews": 1,
+            "user": "6818b69b44dc82c8095cd862",
+            "image": [
+              {"url":"This is test id",
+              "_id":"6818ddbba199ed8135a62be8"}
+            ],
+            "reviews": [],
+            "createdAt": "2025-05-05T15:48:11.905Z",
+            "__v": 12
+        },
+        {
+            "_id": "681d3629cc32e4b0214e2d8f",
+            "name": "water bottle",
+            "description": "1 liter bottle",
+            "price": 300,
+            "ratings": 4,
+            "category": "home",
+            "stock": 1,
+            "numOfReviews": 0,
+            "user": "6818b69b44dc82c8095cd862",
+            "image": [
+                   {"url":"This is test id",
+              "_id":"6818ddbba199ed8135a62be8"}
+            ],
+            "reviews": [],
+            "createdAt": "2025-05-08T22:54:33.351Z",
+            "__v": 0
+        }
+]
 const Home = () => {
   return (
    <>
+   <PageTitle title="Home-My Shop "/>
    <Navbar/>
    <ImageSlider/>
     <div className="home-container">
       <h2 className="home-heading">Trending Now</h2>
+      <div className="home-product-container">
+        {
+        products.length>0 && products.map((product)=>{
+         return (
+         <Product product={product} key={product._id} />
+        )
+        })
+      }
+      </div>
 
     </div>
 <Footer/>    
