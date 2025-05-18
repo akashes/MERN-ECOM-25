55480985b2c17757bf7a20fcabb2eaee76516df7
akash <akashes5753279@gmail.com>
2025-05-18 11:10:59 +0530

    feat:add login component and login logic in the redux

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import {Routes,BrowserRouter as Router , Route } from 'react-router-dom'
 import ProductPage from './pages/ProductPage'
 import Products from './pages/Products'
 import Register from './User/Register'
-
+import Login from './User/Login'
 const Home = React.lazy(() => import('./pages/Home'))
 const App = () => {
   return (
@@ -14,6 +14,7 @@ const App = () => {
     <Route path='/product/:id' element={<ProductPage/>} />
     <Route path='/products' element={<Products/>} />
     <Route path='/register' element={<Register/>} />
+    <Route path='/login' element={<Login/>} />
 
   </Routes>
       </Suspense>
--- frontend/src/features/user/userSlice.js
+++ frontend/src/features/user/userSlice.js
@@ -18,7 +18,7 @@ const register = createAsyncThunk('user/register',async(userData,{rejectWithValu
             }
         }
        const {data} = await axios.post('/api/v1/register',userData,config)
-       console.log('registration data')
+       console.log('registration data',data)
        return data
     } catch (error) {
         console.log(error)
@@ -30,6 +30,23 @@ const register = createAsyncThunk('user/register',async(userData,{rejectWithValu
 
 })
 
+const login = createAsyncThunk('user/login',async({email,password},{rejectWithValue})=>{
+    try {
+        const config={
+            headers:{
+                'Content-Type':'application/json'
+            }
+        }
+       const {data} = await axios.post('/api/v1/login',{email,password},config)
+       console.log('login data',data)
+       return data
+
+    } catch (error) {
+        console.log(error)
+        return rejectWithValue(error.response?.data || 'Login failed. please try again later')
+    }
+    
+})
 
 const userSlice=createSlice({
     name:'user',
@@ -68,8 +85,27 @@ const userSlice=createSlice({
             state.user=null,
             state.isAuthenticated=false
         })
+        //login cases //same as register logic ,no change
+        builder.addCase(login.pending,(state)=>{
+            state.loading=true
+            state.error=null
+        })
+        .addCase(login.fulfilled,(state,action)=>{
+            state.loading=false,
+            state.error=null,
+            state.success=action.payload.success,
+            state.user=action.payload?.user || null ,
+            state.isAuthenticated=Boolean(action.payload?.user)
+            console.log(state.user)
+        })
+        .addCase(login.rejected,(state,action)=>{
+            state.loading=false,
+            state.error = action.payload?.message || 'Registration failed. please try again later'
+            state.user=null,
+            state.isAuthenticated=false
+        })
     }
 })
-export {register}
+export {register,login}
 export const  {removeErrors,removeSuccess} = userSlice.actions
 export default userSlice.reducer
