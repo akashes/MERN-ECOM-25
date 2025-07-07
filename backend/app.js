e17c0fe329e51eaeceb426d28db0cd6f13b64603
akash <akashes5753279@gmail.com>
2025-07-07 21:16:18 +0530

    fix: code to handle unexpected routes

--- backend/app.js
+++ backend/app.js
@@ -58,7 +58,7 @@ app.use('/api/v1',payment)
 
 //serve static files
 app.use(express.static(path.join(__dirname,'../frontend/dist')))
-app.get('/*', (_, res) => {
+app.get('/{*splat}', (req, res) => {
   res.sendFile(path.resolve(__dirname, '../frontend/dist/index.html'));
 });
 
